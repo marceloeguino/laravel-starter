c4e23db4687eeb24fe8789a0943c65d326652a2c
Marcelo Eguino <meguino@nubity.com>
2025-09-11 20:12:58 -0400

    Test Github Webhook

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,7 +14,7 @@ class HelloController extends Controller
     public function hello()
     {
         return response()->json([
-            'message' => 'Hello World Chelo!',
+            'message' => 'Hello World Chelo Test!',
             'status' => 'success',
             'timestamp' => now()->toISOString()
         ]);
