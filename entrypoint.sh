820626694e6baa0a6a574ebe90670b72cb55490e
Marcelo Eguino <meguino@nubity.com>
2025-09-18 09:17:56 -0400

    Update escape character

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Replace values in .env file with runtime env vars
-[ -n "${APP_ENV}" ] && sed -i "s/^APP_ENV=.*/APP_ENV=${APP_ENV}/" /var/www/html/.env
-[ -n "${APP_KEY}" ] && sed -i "s/^APP_KEY=.*/APP_KEY=${APP_KEY}/" /var/www/html/.env
+[ -n "${APP_ENV}" ] && sed -i "s|^APP_ENV=.*|APP_ENV=${APP_ENV}/" /var/www/html/.env
+[ -n "${APP_KEY}" ] && sed -i "s|^APP_KEY=.*|APP_KEY=${APP_KEY}/" /var/www/html/.env
 [ -n "${APP_URL}" ] && sed -i "s|^APP_URL=.*|APP_URL=${APP_URL}|" /var/www/html/.env
 
 # Clear and cache config
